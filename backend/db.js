609f8b13251bb797b9aeae5fbbecde7776246102
Uyennhii132 <nhi_2251220245@dau.edu.vn>
2026-06-19 15:34:59 +0700

    Add MySQL connection log

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD || process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect((err) => {
     if (err) {
         console.error("❌ MySQL connection error:", err);
         return;
     }
+
     console.log("✅ Connected to MySQL");
-    console.log("HOST:", process.env.MYSQLHOST);
-    console.log("PORT:", process.env.MYSQLPORT);
-    console.log("DB:", process.env.MYSQLDATABASE);
 });
 
 module.exports = db;

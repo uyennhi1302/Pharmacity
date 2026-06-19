cdc6798b1ed0bf960d612dfbae38c5555a53b806
Uyennhii132 <nhi_2251220245@dau.edu.vn>
2026-06-19 14:14:12 +0700

    Prepare for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -104,6 +104,8 @@ app.post("/add-product", (req, res) => {
     });
 });
 
-app.listen(5001, () => {
-    console.log("Server running on port 5001");
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });

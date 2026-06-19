893ae407514137b41eab341c85789a7c417a63e3
Uyennhii132 <nhi_2251220245@dau.edu.vn>
2026-06-19 15:46:55 +0700

    Fix db connection

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,22 @@
 const mysql = require("mysql2");
 
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_NAME =", process.env.DB_NAME);
+
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    port: Number(process.env.MYSQLPORT),
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectTimeout: 20000
 });
 
 db.connect((err) => {
     if (err) {
-        console.error("❌ MySQL connection error:", err.message);
+        console.error("❌ MySQL connection error:", err);
     } else {
         console.log("✅ Connected to MySQL");
     }

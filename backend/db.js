a63358456395936cc460544923e8e8bfd26667ea
Uyennhii132 <nhi_2251220245@dau.edu.vn>
2026-06-19 14:12:31 +0700

    Fix render deployment

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,11 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "shopdb"
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log("MySQL Connected");
-});
-
-module.exports = connection;
+module.exports = db;
